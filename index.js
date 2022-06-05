cf6b2c05346b9125183ec7de9a3cf08ccf09dbe5
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-06 00:23:13 +0300

    Simple Counter

---
+++ index.js
@@ -0,0 +1,12 @@
+  var data = 0;
+
+      document.getElementById("counting").innerText = data;
+
+      function increase() {
+        data = data + 1;
+        document.getElementById("counting").innerText = data;
+      }
+      function decrease() {
+        data = data - 1;
+        document.getElementById("counting").innerText = data;
+      }
